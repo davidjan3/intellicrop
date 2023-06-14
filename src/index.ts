de77b8e95073e2cb06bff8e184386d0449f56892
David.Jandrey <david.jandrey@scopevisio.com>
2023-06-14 11:21:16 +0200

    Add&Configure prettier, lint and prepared jest

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import Cropper from "./cropper/cropper.js";
 
-const fileUploadEl: any = document.getElementById("file-upload");
-const srcImgEl: any = document.getElementById("src-image");
-const theCanvas: any = document.getElementById("the-canvas");
-const theOtherCanvas: any = document.getElementById("the-other-canvas");
-const theButton: any = document.getElementById("the-button");
+const fileUploadEl = document.getElementById("file-upload");
+const srcImgEl = document.getElementById("src-image") as HTMLImageElement;
+const theCanvas = document.getElementById("the-canvas") as HTMLCanvasElement;
+const theOtherCanvas = document.getElementById("the-other-canvas") as HTMLCanvasElement;
+const theButton = document.getElementById("the-button");
 
 fileUploadEl?.addEventListener(
   "change",
   function (e) {
-    srcImgEl.src = URL.createObjectURL(e.target.files[0]);
+    srcImgEl.src = URL.createObjectURL((e.target as HTMLInputElement).files[0]);
   },
   false
 );
